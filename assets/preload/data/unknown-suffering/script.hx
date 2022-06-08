b5a2b11ac509cf44280e55c9c278b4cb03ec56c2
LeanDapper <84594436+LeanDapper@users.noreply.github.com>
2022-06-08 15:25:43 -0700

    shithead

---
+++ assets/preload/data/unknown-suffering/script.hx
@@ -0,0 +1,11 @@
+function onStepHit()
+{
+    switch(curStep)
+    {
+        case 1610:
+            PlayState.addCinematicBars(0.01);
+            PlayState.camHUD.alpha = 0;
+        case 1744:
+            FlxTween.tween(camHUD, {alpha: 1}, 0.5);
+    }
+}
