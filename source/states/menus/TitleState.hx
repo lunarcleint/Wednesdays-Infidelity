2f323e75f1684b198ade2a27bb0e8f790ebb01fb
lunarcleint <88809950+lunarcleint@users.noreply.github.com>
2022-06-26 21:06:43 -0700

    Restored Old Title State

--- source/states/menus/TitleState.hx
+++ source/states/menus/TitleState.hx
@@ -47,15 +47,10 @@ class TitleState extends MusicBeatState
 	var credGroup:FlxGroup;
 	var credTextShit:Alphabet;
 	var textGroup:FlxGroup;
-	var logoSpr:FlxSprite;
+
+	var ngSpr:FlxSprite;
 
 	var curWacky:Array<String> = [];
-
-	var wackyImage:FlxSprite;
-
-	var easterEggEnabled:Bool = true; // Disable this to hide the easter egg
-	var easterEggKeyCombination:Array<FlxKey> = [FlxKey.B, FlxKey.B]; // bb stands for bbpanzu cuz he wanted this lmao
-	var lastKeysPressed:Array<FlxKey> = [];
 
 	var mustUpdate:Bool = false;
 
@@ -154,13 +149,13 @@ class TitleState extends MusicBeatState
 
 		credTextShit.visible = false;
 
-		logoSpr = new FlxSprite(0, FlxG.height * 0.4).loadGraphic(Paths.image('titlelogo'));
-		add(logoSpr);
-		logoSpr.visible = false;
-		logoSpr.setGraphicSize(Std.int(logoSpr.width * 0.55));
-		logoSpr.updateHitbox();
-		logoSpr.screenCenter(X);
-		logoSpr.antialiasing = ClientPrefs.globalAntialiasing;
+		ngSpr = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('newgrounds_logo'));
+		add(ngSpr);
+		ngSpr.visible = false;
+		ngSpr.setGraphicSize(Std.int(ngSpr.width * 0.8));
+		ngSpr.updateHitbox();
+		ngSpr.screenCenter(X);
+		ngSpr.antialiasing = ClientPrefs.globalAntialiasing;
 
 		// FlxG.camera.shake(0.004, 4000);
 
@@ -227,8 +222,6 @@ class TitleState extends MusicBeatState
 			#end
 		}
 
-		// EASTER EGG
-
 		if (!transitioning && skippedIntro)
 		{
 			if (pressedEnter)
@@ -261,54 +254,6 @@ class TitleState extends MusicBeatState
 					closedState = true;
 				});
 				// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
-			}
-			else if (easterEggEnabled)
-			{
-				var finalKey:FlxKey = FlxG.keys.firstJustPressed();
-				if (finalKey != FlxKey.NONE)
-				{
-					lastKeysPressed.push(finalKey); // Convert int to FlxKey
-					if (lastKeysPressed.length > easterEggKeyCombination.length)
-					{
-						lastKeysPressed.shift();
-					}
-
-					if (lastKeysPressed.length == easterEggKeyCombination.length)
-					{
-						var isDifferent:Bool = false;
-						for (i in 0...lastKeysPressed.length)
-						{
-							if (lastKeysPressed[i] != easterEggKeyCombination[i])
-							{
-								isDifferent = true;
-								break;
-							}
-						}
-
-						if (!isDifferent)
-						{
-							trace('Easter egg triggered!');
-							FlxG.save.data.psykaEasterEgg = !FlxG.save.data.psykaEasterEgg;
-							FlxG.sound.play(Paths.sound('secretSound'));
-
-							var black:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width * 2, FlxG.height * 2, FlxColor.BLACK);
-							black.alpha = 0;
-							add(black);
-
-							FlxTween.tween(black, {alpha: 1}, 1, {
-								onComplete: function(twn:FlxTween)
-								{
-									FlxTransitionableState.skipNextTransIn = true;
-									FlxTransitionableState.skipNextTransOut = true;
-									MusicBeatState.switchState(new TitleState());
-								}
-							});
-							lastKeysPressed = [];
-							closedState = true;
-							transitioning = true;
-						}
-					}
-				}
 			}
 		}
 
@@ -368,11 +313,10 @@ class TitleState extends MusicBeatState
 			switch (curStep)
 			{
 				case 4:
-					createCoolText(['Psych Engine by'], 45);
+					createCoolText(['ninjamuffin99', 'phantomArcade', 'kawaisprite', 'evilsk8er']);
 				// credTextShit.visible = true;
 				case 12:
-					addMoreText('Shadow Mario', 45);
-					addMoreText('RiverOaken', 45);
+					addMoreText('present');
 				// credTextShit.text += '\npresent...';
 				// credTextShit.addText();
 				case 16:
@@ -381,23 +325,23 @@ class TitleState extends MusicBeatState
 				// credTextShit.text = 'In association \nwith';
 				// credTextShit.screenCenter();
 				case 20:
-					createCoolText(['This is a mod to'], -60);
+					createCoolText(['In association', 'with']);
 				case 28:
-					addMoreText('This game right below lol', -60);
-					logoSpr.visible = true;
+					addMoreText('newgrounds');
+					ngSpr.visible = true;
 				// credTextShit.text += '\nNewgrounds';
 				case 32:
 					deleteCoolText();
-					logoSpr.visible = false;
+					ngSpr.visible = false;
 				// credTextShit.visible = false;
 
 				// credTextShit.text = 'Shoutouts Tom Fulp';
 				// credTextShit.screenCenter();
 				case 36:
-					addMoreText("Eso tilin");
+					addMoreText("The wait is over");
 				// credTextShit.visible = true;
 				case 44:
-					addMoreText("Vaya tilin");
+					addMoreText("Its finally here");
 				// credTextShit.text += '\nlmao';
 				case 48:
 					deleteCoolText();
@@ -434,7 +378,7 @@ class TitleState extends MusicBeatState
 	{
 		if (!skippedIntro)
 		{
-			remove(logoSpr);
+			remove(ngSpr);
 
 			// if (ClientPrefs.flashing)
 			FlxG.camera.flash(FlxColor.BLACK, 2.3, null, true);
