a5d17423e178393cbb3ad26308d236362f1910b4
LeanDapper <84594436+LeanDapper@users.noreply.github.com>
2022-08-25 18:53:19 -0700

    new icons

---
+++ assets/preload/data/accelerant/script.hx
@@ -0,0 +1,14 @@
+function onStepHit()
+{
+	switch (curStep)
+	{
+		case 703:
+			PlayState.changeDadIcon(true);
+		case 831:
+			PlayState.changeDadIcon(false);
+		case 895:
+			PlayState.changeDadIcon(true);
+		case 959:
+			PlayState.changeDadIcon(false);
+	}
+}
