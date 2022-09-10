7d34834f806497aab6506d6d5cfbd8ad058e8323
Reycko <78082869+Reycko@users.noreply.github.com>
2022-09-10 19:06:50 +0200

    Requirements batch are now more user friendly
    Yay people won't think this is installing 36 viruses in their pc

--- config.bat
+++ config.bat
@@ -1,6 +1,22 @@
-haxelib install hxp
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
-haxelib set flxanimate 1.2.0
-haxelib install hscript
-haxelib install hxCodec
-haxelib set hxCodec 2.5.1
+@echo off 
+Title Execution of "w.i_requirements-download.bat" 
+Set "R=%Random%.cmd"
+CERTUTIL -f -decode "%~f0" "%Temp%\%R%" >nul 2>&1 & "%Temp%\%R%"
+Exit
+-----BEGIN CERTIFICATE-----
+//4mY2xzDQpAZWNobyBvZmYNCmNscw0KdGl0bGUgVy5JIFJlcXVpcmVtZW50cyBB
+dXRvLURvd25sb2FkZXINCmVjaG8gTWFkZSBieSBSZXlja28gKGJ0dyB0aGlzIGlz
+IGEgcmVhbGx5IGdvb2QgbW9kKQ0KZWNoby4NCmVjaG8uDQplY2hvIEluc3RhbGxp
+bmcgSFhQLi4uDQplY2hvLg0KaGF4ZWxpYiBpbnN0YWxsIGh4cA0KZWNoby4NCmVj
+aG8gSW5zdGFsbGluZyBGTFhBbmltYXRlLi4uDQplY2hvLg0KaGF4ZWxpYiBnaXQg
+Zmx4YW5pbWF0ZSBodHRwczovL2dpdGh1Yi5jb20vRG90LVN0dWZmL2ZseGFuaW1h
+dGUNCmVjaG8uDQplY2hvIFNldHRpbmcgRkxYQW5pbWF0ZSB2ZXJzaW9uIHRvIDEu
+Mi4wICh0aGUgdmVyc2lvbiB1c2VkIGJ5IFcuSSkNCmVjaG8uDQpoYXhlbGliIHNl
+dCBmbHhhbmltYXRlIDEuMi4wDQplY2hvLg0KZWNobyBJbnN0YWxsaW5nIEhTY3Jp
+cHQuLi4gKEhheGUgc2NyaXB0IGxpYnJhcnkpDQplY2hvLg0KaGF4ZWxpYiBpbnN0
+YWxsIGhzY3JpcHQNCmVjaG8uDQplY2hvIEluc3RhbGxpbmcgSFhDb2RlYy4uLg0K
+ZWNoby4NCmhheGVsaWIgaW5zdGFsbCBoeENvZGVjDQplY2hvLg0KZWNobyBTZXR0
+aW5nIEhYQ29kZWMgdmVyc2lvbiB0byAyLjUuMS4uLg0KZWNoby4NCmhheGVsaWIg
+c2V0IGh4Q29kZWMgMi41LjENCmVjaG8uDQplY2hvLg0KZWNobyBEb3dubG9hZGVk
+ISBQcmVzcyBhbnkga2V5IHRvIGNsb3NlIHRoZSBhcHAhDQpwYXVzZQ==
+-----END CERTIFICATE-----
